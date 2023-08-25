19fe004c49caa4b7ea4d24f151ea63a8da3974e7
aleks <aleksei-davletov@mail.ru>
2023-08-25 12:56:24 +0300

    Выполнил домашнюю работу

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            checkAccount("java_skypro_go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
+        } catch (WrongLoginException e) {
+            System.out.println("Недопустимый логин");
+        } catch (WrongPasswordException e) {
+            System.out.println("Недопустимый пароль");
+        }
+    }
+
+    public static void checkAccount(String login, String password, String confirmPassword) {
+        if (!login.matches("\\w{1,}") || login.length() > 20) {
+            throw new WrongLoginException("Недопустимый логин");
+        } else if (!password.matches("\\w{1,}") || !password.equals(confirmPassword)
+                || password.length() > 20) {
+            throw new WrongPasswordException("Недопустимый пароль");
+        }
     }
 }
