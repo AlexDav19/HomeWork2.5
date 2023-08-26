4b76ce0e9142f32a9856c1016203d20e6e20bed3
aleks <aleksei-davletov@mail.ru>
2023-08-26 12:14:10 +0300

    Добавил finally

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
             System.out.println("Недопустимый логин");
         } catch (WrongPasswordException e) {
             System.out.println("Недопустимый пароль");
+        } finally {
+            System.out.println("Метод выполнен");
         }
     }
 
